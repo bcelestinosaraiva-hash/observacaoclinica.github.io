ce417d6efd89c4b9f2f3a38392e531de7b6b3440
bcelestinosaraiva-hash <bcelestinosaraiva@gmail.com>
2026-08-26 14:06:32 +0200

    scccsd

--- webpush.mjs
+++ webpush.mjs
@@ -1,6 +1,6 @@
 import { notifyNewArticles } from "./scripts/push-notify.mjs";
 await notifyNewArticles([{
-    title: "Observação clínica: o que é, para que serve e como é feita",
-    url: "https://observacaoclinica.com/saude-az/observacao-clinica-o-que-e/",
-    image: "https://observacaoclinica.com/img/observacao-clinica-o-que-e-748.webp"
+    title: "Glossário clínico",
+    url: "https://observacaoclinica.com/saude-az/glossario-clinico/",
+    image: "https://observacaoclinica.com/img/glossario-1200.webp"
 }]);
