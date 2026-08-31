83df5f025b6a870f32abaae9405aad45df466ea4
bcelestinosaraiva-hash <bcelestinosaraiva@gmail.com>
2026-08-31 12:35:00 +0200

    gravidez um artigo

--- webpush.mjs
+++ webpush.mjs
@@ -1,6 +1,6 @@
 import { notifyNewArticles } from "./scripts/push-notify.mjs";
 await notifyNewArticles([{
-    title: "Glossário clínico",
+    title: "",
     url: "https://observacaoclinica.com/saude-az/glossario-clinico/",
     image: "https://observacaoclinica.com/img/glossario-1200.webp"
 }]);
